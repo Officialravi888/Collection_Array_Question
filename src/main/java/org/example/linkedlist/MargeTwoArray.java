bb9708acafc779fd61f123d20fac06897f64d073
Officialravi888 <youremail@yourdomain.com>
2023-05-26 16:57:47 +0530

    Add array two

--- src/main/java/org/example/linkedlist/MargeTwoArray.java
+++ src/main/java/org/example/linkedlist/MargeTwoArray.java
@@ -1,4 +1,14 @@
 package org.example.linkedlist;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
 public class MargeTwoArray {
+    public static void main(String[] args) {
+        int a[]={1, 3, 5, 7};
+        int b[]={2, 4, 6};
+      int[] m = IntStream.concat(IntStream.of(a),IntStream.of(b)).toArray();
+        System.out.println(Arrays.toString(m));
+    }
 }
