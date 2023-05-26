45c8f04c5ecdbf3664d15f96a4faae2cc3ca4079
Officialravi888 <youremail@yourdomain.com>
2023-05-26 16:46:55 +0530

    find last no

---
+++ src/main/java/org/example/linkedlist/FindTheElementEndNumber.java
@@ -0,0 +1,12 @@
+package org.example.linkedlist;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class FindTheElementEndNumber {
+    public static void main(String[] args) {
+        LinkedList<Integer>linkedList=new LinkedList<>(Arrays.asList(1,4,5,7,9,0,7,5));
+
+        System.out.println(linkedList);
+    }
+}
--- src/main/java/org/example/linkedlist/ReversElement.java
+++ src/main/java/org/example/linkedlist/ReversElement.java
@@ -1,4 +1,13 @@
 package org.example.linkedlist;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class ReversElement {
+    public static void main(String[] args) {
+        LinkedList<Integer>linkedList=new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        Collections.reverse(linkedList);
+        System.out.println(linkedList);
+    }
 }
